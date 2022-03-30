bf3a8d103b118d13af24cfe988b41a4a4db44fad
kiruxaspb <65371121+kiruxaspb@users.noreply.github.com>
2022-03-30 13:23:07 +0300

    Update task10.sol

--- task10.sol
+++ task10.sol
@@ -1,18 +1,19 @@
-pragma solidity ^0.8.12;
+pragma solidity ^0.8.13;
 
 contract Test {
-    
     uint public value;
     
-    function testFor() public {
+    function testFor(uint _value) public {
         uint counter;
+        value = _value;
         for (uint i = 0; i < value; i++) {
             counter += value;
         }
     }
 
-    function testWhile() public {
+    function testWhile(uint _value) public {
         uint i;
+        i = _value;
         while (i != value) {
             uint counter;
             counter += value;
