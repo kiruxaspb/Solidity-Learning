befd067d8a68fc2fb83bea7244a355a4861e4f8e
kiruxaspb <dvoreckovkirill@gmail.com>
2022-03-31 23:54:49 +0300

    Update task10.sol

--- task10.sol
+++ task10.sol
@@ -1,23 +1,40 @@
-pragma solidity ^0.8.13;
+/*
+Задача №10
+Новый этап - освоение контрактов для Tron и BNC
+
+Необходимо: установить контракт с циклом for/while в тестовую сеть TRON
+- установить кошелек TronLink,
+- получить некоторое количество TRX.
+А также отписаться сюда и сделать пометку комментарием в коде контракта
+на каком шаге итерации перестает работать цикл.
+
+Попробовать тоже самое сделать для системы Binance Smart Chain
+*/
+
+pragma solidity ^0.8.6;
 
 contract Test {
-    uint public value;
-    
-    function testFor(uint _value) public {
-        uint counter;
-        value = _value;
-        for (uint i = 0; i < value; i++) {
-            counter += value;
+    uint public sum;
+
+    function testFor(uint _val1, uint _val2) public {
+        for (uint i = 0; i < _val1; i++) {
+            sum += _val2;
         }
     }
 
-    function testWhile(uint _value) public {
+    function testWhile(uint _val1, uint _val2) public {
         uint i;
-        i = _value;
-        while (i != value) {
-            uint counter;
-            counter += value;
+        while (i != _val1) {
+            sum += _val2;
             i++;
         }
+    }
+
+    function showValue() public view returns (uint) {
+        return sum;
+    }
+
+    function clearValue() public {
+        sum = 0;
     }
 }
