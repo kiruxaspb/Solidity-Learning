382004c2489ea496641b40bf5cf0c9538761cf20
kiruxaspb <dvoreckovkirill@gmail.com>
2022-04-12 18:36:55 +0300

    Create task11.sol

---
+++ task11.sol
@@ -0,0 +1,30 @@
+/*
+Задача №11
+Интерфейсы и модификаторы доступа. Написать контракт работающий с другим контрактом с помощью интерфейса. 
+*/
+
+pragma solidity ^0.8.13;
+
+contract Counter {
+    uint public count;
+
+    function increment() external {
+        count += 1;
+    }
+}
+
+interface ICounter {
+    function count() external view returns (uint);
+
+    function increment() external;
+}
+
+contract MyContract {
+    function incrementCounter(address _counter) external {
+        ICounter(_counter).increment();
+    }
+
+    function getCount(address _counter) external view returns (uint) {
+        return ICounter(_counter).count();
+    }
+}
