250bc5933a771dfd6e6cd01f5853cb00577ac775
kiruxaspb <dvoreckovkirill@gmail.com>
2022-04-14 02:20:42 +0300

    Update task.sol

--- task11.sol
+++ task11.sol
@@ -28,3 +28,32 @@ contract MyContract {
         return ICounter(_counter).count();
     }
 }
+
+
+pragma solidity ^0.8.13;
+
+contract Counter1 {
+    uint public count;
+
+    function increment() external {
+        count += 1;
+    }
+}
+
+interface ICounter1 {
+    function count() external view returns (uint);
+
+    function increment() external;
+}
+
+contract MyContract1 {
+    address public target = 0xd9145CCE52D386f254917e481eB44e9943F39138;
+
+    function incrementCounter() external {
+        ICounter(target).increment();
+    }
+
+    function getCount() external view returns (uint) {
+        return ICounter(target).count();
+    }
+}
