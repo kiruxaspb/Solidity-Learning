8f7f107929436993ae8a268ea6822779370c80f5
kiruxaspb <dvoreckovkirill@gmail.com>
2022-04-16 00:03:57 +0300

    Update task11.sol

--- task11.sol
+++ task11.sol
@@ -3,28 +3,43 @@
 Интерфейсы и модификаторы доступа. Написать контракт работающий с другим контрактом с помощью интерфейса. 
 */
 
+// Platform 
+// https://ropsten.etherscan.io/address/0x672b6c868C3dA4Ef305175c942073cB46A019179#code
+// Verify
+// https://ropsten.etherscan.io/address/0xeA6e735a3AF44448E02FE4D53E85da3b467B4920#code
+
 pragma solidity ^0.8.13;
 
-contract PatentVerify {
-    function verifyPatent() external returns (bool) {
-        return true;
-    }
+contract Verify {
+    bool public verified = true;
+    uint public id;
 
+    function counter() external {
+        id += 1;
+    }
 }
 
 interface Patenter {
-    function verifyPatent() external;
+    function verified() external returns (bool);
+    function id() external returns (uint);
+    function counter() external;
 }
 
-contract PatentPlatform {
-    address private owner;
+contract Platform {
+    address public owner;
 
     constructor() {
         owner = msg.sender;
     }
 
+    modifier onlyOwner() {
+        msg.sender == owner;
+        _;
+    }
+
     mapping(address => Patent) public patents;
     struct Patent {
+        uint patentID;
         string namePatent;
         string description;
         uint patentingTime;
@@ -34,17 +49,29 @@ contract PatentPlatform {
     function PatentRegister(
         string memory name,
         string memory desc,
-        bool valid,
-        address addrPatenter
+        address patentAddress
     ) public {
-        Patenter(addrPatenter).verifyPatent();
+
+        patents[msg.sender].patentID = Patenter(patentAddress).id(); // получаем ID из другого контракта с помощью интерфейса (по адресу контракта)
         patents[msg.sender].namePatent = name;
         patents[msg.sender].description = desc;
         patents[msg.sender].patentingTime = block.timestamp;
-        patents[msg.sender].verify = valid;
+
+        patents[msg.sender].verify = Patenter(patentAddress).verified(); // получаем переменную из другого контракта
+
+        Patenter(patentAddress).counter(); // задействуем удаленную функцию
     }
 }
 
+
+
+
+
+
+
+
+
+/*
 pragma solidity ^0.8.13;
 
 contract Counter {
@@ -99,3 +126,4 @@ contract MyContract1 {
         return ICounter(target).count();
     }
 }
+*/
