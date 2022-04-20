5c22de4eb8ea9f4fa91200cac1bb5a959f6463d0
kiruxaspb <dvoreckovkirill@gmail.com>
2022-04-20 14:09:12 +0300

    Update task12.sol

--- task12.sol
+++ task12.sol
@@ -1,1 +1,47 @@
+/*
+Задача №12
+Конструкторы и fallback-функции.
+Рассмотреть, как они реализованы в разных версиях Solidity - привести примеры и комментарий к каждому примеру. 
+Написать простейший контракт с конструктором и fallback-функцией для 7-ой версии
+*/
 
+
+// конструкторы
+// 0.5.17
+
+// 0.6.12
+
+// 0.7.6
+
+// 0.8.13
+
+
+// fallback funcs
+// 0.5.17
+
+// 0.6.12
+
+// 0.7.6
+
+// 0.8.13
+
+pragma solidity ^0.7.0;
+
+contract Base {
+    uint x;
+    constructor(uint _x) { x = _x; }
+
+    function getX() public view returns (uint) {
+        return x;
+    }
+}
+
+// Either directly specify in the inheritance list...
+contract Derived1 is Base(7) {
+    constructor() {}
+}
+
+// or through a "modifier" of the derived constructor.
+contract Derived2 is Base {
+    constructor(uint _y) Base(_y * _y) {}
+}
