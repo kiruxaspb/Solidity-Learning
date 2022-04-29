21be0b76a6dc4a778534e146a1ac4af8bd289a21
kiruxaspb <dvoreckovkirill@gmail.com>
2022-04-30 00:15:32 +0300

    Edit code style

--- Task15.sol
+++ Task15.sol
@@ -4,25 +4,42 @@
 Написать пример, когда при установке главного контракта в связанном контракте
 выполняется конструктор с параметрами из главного контракта
 */
+
+// MyContract1
+// https://ropsten.etherscan.io/address/0x5bF5C865d11ae3b4E164DC61718bec16BDe652D7#code
+
+// MyContract2
+// https://ropsten.etherscan.io/address/0xb352732f32867fBad42d76Dda276A76235891DdB#code
+
+// MyContract3
+// https://ropsten.etherscan.io/address/0x009aB693458841370337cFa7A4E33d8E15B0437D#code
+
 pragma solidity ^0.8.13;
 
 contract MyContract1 {
     address owner;
+
     constructor() {
         owner = msg.sender;
     }
+
+    function getContractOnwer() public view returns (address) {
+        return owner;
+    }
 }
+
 
 contract MyContract2 is MyContract1 {
     address public creator;
-    uint public value;
-    constructor(uint _value) {
-        value = _value;
+    string public name;
+
+    constructor(string memory _name) {
+        name = _name;
         creator = owner;
     }
 
-    function getValue2() public view returns (uint) {
-        return value;
+    function getOwnerName() public view returns (string memory) {
+        return name;
     }
 
     function getOwner2() public view returns (address) {
@@ -30,18 +47,20 @@ contract MyContract2 is MyContract1 {
     }
 }
 
+
 contract MyContract3 is MyContract1, MyContract2 {
-    uint test;
-    address creator3;
-    constructor() MyContract1() MyContract2(22) {
-        test = value;
+    string public testname;
+    address public creator3;
+    
+    constructor() MyContract1() MyContract2("Kirill") {
+        testname = name;
         creator3 = owner;
     }
     function getOwner3() public view returns (address) {
         return creator3;
     }
 
-    function getValue3() public view returns (uint) {
-        return test;
+    function getOwnerName3() public view returns (string memory) {
+        return testname;
     }
 }
